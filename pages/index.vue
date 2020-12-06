ff4f679b257276136e0ffa18c4211009623d1d4f
MystixCode <mystixrage@gmx.ch>
2020-12-06 20:52:20 +0100

    little fix2

--- pages/index.vue
+++ pages/index.vue
@@ -13,23 +13,15 @@
         <p>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
         </p>
-
-
-
       </v-card-text>
     </v-card>
   </v-container>
 </template>
 
 <script>
-import Test from '@/components/Test';
-import UserLogo from "@/components/UserLogo";
-
 export default {
   components: {
 
   }
 }
-
-
 </script>
